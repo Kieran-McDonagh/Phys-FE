748c9cd34817b443988d9e136ffb5d1f5ebd952e
kieran <kieran-mcdonagh@outlook.com>
2024-05-19 03:11:08 +0100

    add bash scripts

---
+++ backend/scripts/run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the Docker Compose test file
+DOCKER_COMPOSE_FILE=docker-compose-tests.yaml
+
+# Build and run the tests using Docker Compose
+docker compose -f $DOCKER_COMPOSE_FILE up --build --abort-on-container-exit
+
+# Capture the exit code of the docker compose command
+EXIT_CODE=$?
+
+# Bring down the Docker Compose services
+docker compose -f $DOCKER_COMPOSE_FILE down
+
+# Exit with the exit code from the tests
+exit $EXIT_CODE
