89dfb22b0ed5f7a4c96e771a2520806dc6c0f138
kieran <117094556+Kieran-McDonagh@users.noreply.github.com>
2024-05-24 15:55:27 +0100

    setting up frontend

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-export default function Index() {
+export default function Index () {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World!</Text>
+    <View style={styles.container}>
+      <Text>Hello World</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container : {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
