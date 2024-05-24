bbd27e998134bbb0eee7a3d74180c6721494572d
kieran <117094556+Kieran-McDonagh@users.noreply.github.com>
2024-05-24 16:24:23 +0100

    testing out screens

--- frontend/app/screens/LoginScreen.tsx
+++ frontend/app/screens/LoginScreen.tsx
@@ -1,16 +1,28 @@
-import React from "react";
-import { View, TouchableOpacity, Text, Alert, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 
 const LoginScreen = () => {
-  const handleClick = () => {
-    Alert.alert("Button clicked");
+  const [counter, setCounter] = useState(0);
+
+  const increaseCount = () => {
+    setCounter(counter + 1);
+  };
+
+  const resetCount = () => {
+    setCounter(0);
   };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={handleClick}>
+      <Text style={styles.counterText}>Counter: {counter}</Text>
+      <TouchableOpacity onPress={increaseCount}>
         <View style={styles.button}>
-          <Text style={styles.buttonText}>Click me</Text>
+          <Text style={styles.buttonText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={resetCount}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Reset</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -27,9 +39,15 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: "blue",
     borderRadius: 5,
+    marginTop: 10,
+    width: 100,
   },
   buttonText: {
     color: "white",
+    margin: "auto",
+  },
+  counterText: {
+    fontSize: 18,
   },
 });
 
